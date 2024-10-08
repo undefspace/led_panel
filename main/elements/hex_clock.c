04c0c3b4bf571e0ad92820a0f20353131b79c16f
Anna Antonenko <portasynthinca3@gmail.com>
2024-10-08 13:44:32 +0300

    feat: honor 0x1b :saluting_face:

--- main/elements/hex_clock.c
+++ main/elements/hex_clock.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 #include "fonts/small_clock.h"
+#include "elements/spectrum.h"
+#include <esp_timer.h>
 
 void hex_clock_draw(Olivec_Canvas canvas) {
     // get time
@@ -9,7 +11,16 @@ void hex_clock_draw(Olivec_Canvas canvas) {
     time(&now);
     struct tm time;
     localtime_r(&now, &time);
-    
+
+    uint32_t minutes_color;
+    if(time.tm_min == 0x1B) {
+        // pay respects to the previous name of this awesome hackerspace, 0x1B
+        float time = esp_timer_get_time() / 1000000.0f / HEX_CLOCK_0x1B_ANIMATION_PERIOD;
+        minutes_color = angle_to_color(time);
+    } else {
+        minutes_color = HEX_CLOCK_FG;
+    }
+
     // print time
     char buf[6];
     sprintf(buf, "%02X", time.tm_hour);
@@ -17,5 +28,5 @@ void hex_clock_draw(Olivec_Canvas canvas) {
     olivec_text(canvas, "0x", 0, 0, small_clock, HEX_CLOCK_0x_FG);
     olivec_text(canvas, buf, 0, 6, small_clock, HEX_CLOCK_FG);
     olivec_text(canvas, "0x", 0, 16, small_clock, HEX_CLOCK_0x_FG);
-    olivec_text(canvas, buf + 3, 0, 22, small_clock, HEX_CLOCK_FG);
+    olivec_text(canvas, buf + 3, 0, 22, small_clock, minutes_color);
 }
--- main/include/elements/hex_clock.h
+++ main/include/elements/hex_clock.h
@@ -2,8 +2,10 @@
 
 #include "ui_element.h"
 
-#define HEX_CLOCK_0x_FG 0xff202020
+#define HEX_CLOCK_0x_FG 0xff404040
 #define HEX_CLOCK_FG    0xffffffff
+
+#define HEX_CLOCK_0x1B_ANIMATION_PERIOD 5.0f
 
 void hex_clock_draw(Olivec_Canvas canvas);
 static const ui_element_t hex_clock = {
